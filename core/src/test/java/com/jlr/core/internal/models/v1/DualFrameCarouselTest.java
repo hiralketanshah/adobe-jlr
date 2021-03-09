a1bde680ba28ba5f55539efb1ea7157617745a80
RanjitBalhara <tec88233@adobe.com>
2021-03-09 14:30:24 +0530

    [feature/kestrel-29] JUnit added
    [feature/kestrel-29] JUnit added

---
+++ core/src/test/java/com/jlr/core/internal/models/v1/DualFrameCarouselTest.java
@@ -0,0 +1,56 @@
+package com.jlr.core.internal.models.v1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.apache.sling.api.resource.Resource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import com.jlr.core.pojos.CTAPojo;
+
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+
+@ExtendWith(AemContextExtension.class)
+class DualFrameCarouselTest {
+
+    private DualFrameCarouselImpl dualFrameModel;
+
+    @BeforeEach
+    void setUp(AemContext context) throws Exception {
+        context.load().json("/content/jlr/dualframecarousel/dualframecarousel.json", "/content/jlr/duelframecarousel.html");
+        Resource resource = context.resourceResolver().getResource("/content/jlr/duelframecarousel.html");
+        dualFrameModel = resource.adaptTo(DualFrameCarouselImpl.class);
+    }
+
+    @Test
+    void testGeneralProperties() {
+        assertEquals("12345", dualFrameModel.getId());
+        assertEquals("test_header_Title", dualFrameModel.getHeaderTitle());
+        assertEquals("test_body_copy", dualFrameModel.getCopy());
+    }
+
+    @Test
+    void testImageProperties() {
+        assertEquals("/content/dam/test.png", dualFrameModel.getFileReference());
+        assertEquals("test_imageAlt", dualFrameModel.getImageAlt());
+        assertEquals("/content/jlr/au", dualFrameModel.getImageLink());
+    }
+
+    @Test
+    void testCtaProperties() {
+		List<CTAPojo> list = dualFrameModel.getCtaList();
+        assertEquals(1, list.size());
+        list.forEach(item -> { 
+        assertEquals("test_cta_text", item.getText());
+        assertEquals("/content/jlr/au", item.getLink());
+        assertEquals("_self", item.getTarget());
+        assertEquals("primary", item.getLinkType());
+        });
+    
+    }
+
+}
