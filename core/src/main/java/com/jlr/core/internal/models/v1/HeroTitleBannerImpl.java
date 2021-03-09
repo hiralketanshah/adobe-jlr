9558d6475cc7c06764eb0bccbf1896a9dd0ae27d
Hiral Shah <hishah@adobe.com>
2021-03-09 14:56:52 +0530

    [feature/kestrel-35] Added implementation for GlobalModel.java and updated all other components accordingly

--- core/src/main/java/com/jlr/core/internal/models/v1/HeroTitleBannerImpl.java
+++ core/src/main/java/com/jlr/core/internal/models/v1/HeroTitleBannerImpl.java
@@ -12,11 +12,8 @@ import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Optional;
-import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
-import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 import com.jlr.core.constants.CommonConstants;
-import com.jlr.core.models.GlobalModel;
 import com.jlr.core.models.HeroTitleBannerModel;
 import com.jlr.core.pojos.CTAPojo;
 import com.jlr.core.utils.LinkUtils;
@@ -24,64 +21,44 @@ import com.jlr.core.utils.LinkUtils;
 /**
  * The Class HeroTitleBannerImpl.
  */
-@Model(adaptables = Resource.class, adapters = { HeroTitleBannerModel.class, GlobalModel.class }, resourceType = HeroTitleBannerImpl.RESOURCE_TYPE)
-public class HeroTitleBannerImpl implements GlobalModel, HeroTitleBannerModel{
-	
-	/** The Constant RESOURCE_TYPE. */
-	public static final String RESOURCE_TYPE = "jlr/components/heroTitleBanner/v1/heroTitleBanner";
-	
-	/** The id. */
-	@ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    private String id;
+@Model(adaptables = Resource.class, adapters = {
+        HeroTitleBannerModel.class }, resourceType = HeroTitleBannerImpl.RESOURCE_TYPE)
+public class HeroTitleBannerImpl extends GlobalModelImpl implements HeroTitleBannerModel {
 
-    /** The header title. */
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    private String headerTitle;
+    /** The Constant RESOURCE_TYPE. */
+    public static final String RESOURCE_TYPE = "jlr/components/heroTitleBanner/v1/heroTitleBanner";
 
-    /** The copy. */
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    private String copy;
-
-    /** The file reference. */
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    private String fileReference;
-
-    /** The image alt. */
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    private String imageAlt;
-    
     /** The hero cta. */
     @Inject
     @Optional
     private Resource heroCta;
-    
+
     /** The resource resolver. */
     @Inject
     private ResourceResolver resourceResolver;
-    
+
     /** The list. */
     List<CTAPojo> list = new ArrayList<>();
 
-	/**
-	 * Inits the.
-	 */
-	@PostConstruct
+    /**
+     * Inits the.
+     */
+    @PostConstruct
     public void init() {
         if (heroCta != null && heroCta.hasChildren()) {
             Iterator<Resource> childResources = heroCta.listChildren();
             while (childResources.hasNext()) {
                 Resource child = childResources.next();
                 ValueMap properties = child.adaptTo(ValueMap.class);
-                list.add(
-                        new CTAPojo(properties.get(CommonConstants.PN_CTA_TEXT, String.class),
-                                LinkUtils.appendLinkExtension(properties.get(CommonConstants.PN_CTA_LINK, String.class),
-                                        resourceResolver),
-                                properties.get(CommonConstants.PN_CTA_TARGET, String.class),
-                                properties.get(CommonConstants.PN_CTA_LINK_TYPE, String.class)));
+                list.add(new CTAPojo(properties.get(CommonConstants.PN_CTA_TEXT, String.class),
+                        LinkUtils.appendLinkExtension(properties.get(CommonConstants.PN_CTA_LINK, String.class),
+                                resourceResolver),
+                        properties.get(CommonConstants.PN_CTA_TARGET, String.class),
+                        properties.get(CommonConstants.PN_CTA_LINK_TYPE, String.class)));
             }
         }
     }
-    
+
     /**
      * Gets the hero cta.
      *
@@ -91,55 +68,4 @@ public class HeroTitleBannerImpl implements GlobalModel, HeroTitleBannerModel{
     public List<CTAPojo> getHeroCta() {
         return list;
     }
-    
-    /**
-     * Gets the id.
-     *
-     * @return the id
-     */
-    @Override
-    public String getId() {
-		return id;
-	}
-    
-    /**
-     * Gets the header copy.
-     *
-     * @return the header copy
-     */
-    @Override
-	public String getHeaderTitle() {
-		return headerTitle;
-	}
-	
-    /**
-     * Gets the copy.
-     *
-     * @return the copy
-     */
-    @Override
-	public String getCopy() {
-		return copy;
-	}
-    
-    /**
-     * Gets the file reference.
-     *
-     * @return the file reference
-     */
-    @Override
-	public String getFileReference() {
-		return fileReference;
-	}
-    
-    /**
-     * Gets the image alt.
-     *
-     * @return the image alt
-     */
-    @Override
-	public String getImageAlt() {
-		return imageAlt;
-	}
-
 }
