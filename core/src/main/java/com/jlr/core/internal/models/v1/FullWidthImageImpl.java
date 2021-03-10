9fcfc65953424288030c37a8d1c395560de9f9e7
tec38572@adobe.com <tec38572@adobe.com>
2021-03-10 17:27:25 +0530

    Feature/kestrel 76 (#29)
    * Full Width Image Component created
    
    * Javadoc added

---
+++ core/src/main/java/com/jlr/core/internal/models/v1/FullWidthImageImpl.java
@@ -0,0 +1,91 @@
+package com.jlr.core.internal.models.v1;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import com.jlr.core.constants.CommonConstants;
+import com.jlr.core.models.FullWidthImageModel;
+import com.jlr.core.pojos.CTAPojo;
+import com.jlr.core.utils.LinkUtils;
+
+/**
+ * The Class FullWidthImageImpl.
+ */
+@Model(adaptables = Resource.class, adapters = {
+		FullWidthImageModel.class }, resourceType = FullWidthImageImpl.RESOURCE_TYPE)
+public class FullWidthImageImpl extends GlobalModelImpl implements FullWidthImageModel {
+	
+	/** The Constant RESOURCE_TYPE. */
+	public static final String RESOURCE_TYPE = "jlr/components/fullwidthimage/v1/fullwidthimage";
+	
+	/** The cta list. */
+    @Inject
+    @Optional
+    private Resource ctaList;
+
+    /** The resource resolver. */
+    @Inject
+    private ResourceResolver resourceResolver;
+
+    /** The list. */
+    List<CTAPojo> list = new ArrayList<>();
+    
+    /** The body copy. */
+    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+    private String bodyCopy;
+
+
+
+	/**
+     * Inits the.
+     */
+    @PostConstruct
+    public void init() {
+        if (null != ctaList && ctaList.hasChildren()) {
+            Iterator<Resource> childResources = ctaList.listChildren();
+            while (childResources.hasNext()) {
+                Resource child = childResources.next();
+                ValueMap properties = child.adaptTo(ValueMap.class);
+                list.add(new CTAPojo(properties.get(CommonConstants.PN_CTA_TEXT, String.class),
+                        LinkUtils.appendLinkExtension(properties.get(CommonConstants.PN_CTA_LINK, String.class),
+                                resourceResolver),
+                        properties.get(CommonConstants.PN_CTA_TARGET, String.class),
+                        properties.get(CommonConstants.PN_CTA_LINK_TYPE, String.class)));
+            }
+        }
+    }
+
+   
+    /**
+     * Gets the cta list.
+     *
+     * @return the cta list
+     */
+    @Override
+    public List<CTAPojo> getCtaList() {
+        return list;
+    }
+    
+    /**
+     * Gets the body copy.
+     *
+     * @return the body copy
+     */
+    @Override
+    public String getBodyCopy() {
+		return bodyCopy;
+	}
+
+}
