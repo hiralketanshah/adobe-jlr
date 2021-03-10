ff82820b2fc41d04b680da6f3f6d3330d1bd1926
RanjitBalhara <tec88233@adobe.com>
2021-03-10 18:45:50 +0530

    [feature/kestrel-dev] Created the referenced the util method for ctaList
    [feature/kestrel-dev] Created the referenced the util method for ctaList

--- core/src/main/java/com/jlr/core/internal/models/v1/HeroTitleBannerImpl.java
+++ core/src/main/java/com/jlr/core/internal/models/v1/HeroTitleBannerImpl.java
@@ -1,7 +1,6 @@
 package com.jlr.core.internal.models.v1;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -9,14 +8,12 @@ import javax.inject.Inject;
 
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Optional;
 
-import com.jlr.core.constants.CommonConstants;
 import com.jlr.core.models.HeroTitleBannerModel;
 import com.jlr.core.pojos.CTAPojo;
-import com.jlr.core.utils.LinkUtils;
+import com.jlr.core.utils.CtaUtils;
 
 /**
  * The Class HeroTitleBannerImpl.
@@ -46,17 +43,8 @@ public class HeroTitleBannerImpl extends GlobalModelImpl implements HeroTitleBan
     @PostConstruct
     public void init() {
         if (null != ctaList && ctaList.hasChildren()) {
-            Iterator<Resource> childResources = ctaList.listChildren();
-            while (childResources.hasNext()) {
-                Resource child = childResources.next();
-                ValueMap properties = child.adaptTo(ValueMap.class);
-                list.add(new CTAPojo(properties.get(CommonConstants.PN_CTA_TEXT, String.class),
-                        LinkUtils.appendLinkExtension(properties.get(CommonConstants.PN_CTA_LINK, String.class),
-                                resourceResolver),
-                        properties.get(CommonConstants.PN_CTA_TARGET, String.class),
-                        properties.get(CommonConstants.PN_CTA_LINK_TYPE, String.class)));
+            list =  CtaUtils.createCtaList(ctaList, resourceResolver);
             }
-        }
     }
 
    
