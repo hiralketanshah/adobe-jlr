a4f8ccf38684a07ea420feca095ffadf1e98afea
Hiral Shah <hishah@adobe.com>
2021-04-22 22:56:03 +0530

    [feature/kestrel-29] Updated full frame item

--- core/src/main/java/com/jlr/core/models/DualFrameCarouselModel.java
+++ core/src/main/java/com/jlr/core/models/DualFrameCarouselModel.java
@@ -3,20 +3,31 @@ package com.jlr.core.models;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.jlr.core.pojos.CTAPojo;
 
 /**
  * The Interface DualFrameCarouselModel.
  */
 public interface DualFrameCarouselModel {
-	
-	/**
-	 * Gets the hero cta.
-	 *
-	 * @return the hero cta
-	 */
-	default List<CTAPojo> getCtaList() {
+
+    /**
+     * Gets the hero cta.
+     *
+     * @return the hero cta
+     */
+    default List<CTAPojo> getCtaList() {
         return new ArrayList<>();
+    }
+
+    /**
+     * Gets the asset type.
+     *
+     * @return the asset type
+     */
+    default String getAssetType() {
+        return StringUtils.EMPTY;
     }
 
 }
