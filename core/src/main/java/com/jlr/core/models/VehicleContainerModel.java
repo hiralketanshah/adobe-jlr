5b5a7354a45b5f8614db4b3d49e44551226b4a75
Gopinath <tec38572@adobe.com>
2021-04-27 17:15:29 +0530

    KESTREL-507-[Vehicle container model updated]

--- core/src/main/java/com/jlr/core/models/VehicleContainerModel.java
+++ core/src/main/java/com/jlr/core/models/VehicleContainerModel.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ValueMap;
@@ -20,7 +21,7 @@ import com.day.cq.commons.jcr.JcrConstants;
 import com.day.cq.wcm.api.Page;
 import com.jlr.core.constants.CommonConstants;
 
-@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Model(adaptables = {SlingHttpServletRequest.class,Resource.class}, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class VehicleContainerModel {
 	
 	@Inject
@@ -40,18 +41,71 @@ public class VehicleContainerModel {
 	public List<String> getVehicleInfo(){
 		String pagePath=currentPage.getPath();
 		Resource rootNavigationResource=resourceResolver.getResource(pagePath);
-		Resource containerResource=rootNavigationResource.getChild(JcrConstants.JCR_CONTENT).getChild(CommonConstants.JLR_ROOT).getChild(CommonConstants.JLR_CONTAINER);
+		Resource containerResource=rootNavigationResource.getChild(JcrConstants.JCR_CONTENT).getChild(CommonConstants.JLR_ROOT).getChild(CommonConstants.JLR_CONTAINER).getChild("vehiclecontainer").getChild("vehicles");
 		List<String> list = new ArrayList<>();
         Iterator<Resource> childResources=containerResource.listChildren();
-        logger.info("containerResource {}",containerResource);
         while (childResources.hasNext()) {
             Resource child = childResources.next();
             ValueMap properties = child.adaptTo(ValueMap.class);
             if(StringUtils.compare(vehicledropdown, properties.get(JcrResourceConstants.SLING_RESOURCE_TYPE_PROPERTY, String.class))==0) {
             	list.add(properties.get("vehicleTabName", String.class));
+            	logger.info("Inside ifff {}",properties);
             	}
-            logger.info("pagePath {}",pagePath);
-            logger.info("properties {}",properties);
+            }
+        return list;
+	}
+	
+	public List<String> getVehicleFamilyList(){
+		String pagePath=currentPage.getPath();
+		Resource rootNavigationResource=resourceResolver.getResource(pagePath);
+		Resource containerResource=rootNavigationResource.getChild(JcrConstants.JCR_CONTENT).getChild(CommonConstants.JLR_ROOT).getChild(CommonConstants.JLR_CONTAINER).getChild("vehiclecontainer").getChild("vehicles");
+		List<String> list = new ArrayList<>();
+        Iterator<Resource> childResources=containerResource.listChildren();
+        while (childResources.hasNext()) {
+            Resource child = childResources.next();
+            ValueMap properties = child.adaptTo(ValueMap.class);
+            if(StringUtils.compare(vehicledropdown, properties.get(JcrResourceConstants.SLING_RESOURCE_TYPE_PROPERTY, String.class))==0) { 
+            	Resource child2 = child.getChild("vehicleFamily");
+                Iterator<Resource> resource2=child2.listChildren();
+                while(resource2.hasNext()) {
+                	Resource child3 = resource2.next();
+                	ValueMap properties2 = child3.adaptTo(ValueMap.class);
+                	list.add(properties2.get("vehicleFamilyName", String.class));
+                	logger.info("Inside 2ifff {}",properties2);
+                }
+            	}
+            }
+        return list;
+	}
+	
+	public List<String> getVehiclesList(){
+		String pagePath=currentPage.getPath();
+		Resource rootNavigationResource=resourceResolver.getResource(pagePath);
+		Resource containerResource=rootNavigationResource.getChild(JcrConstants.JCR_CONTENT).getChild(CommonConstants.JLR_ROOT).getChild(CommonConstants.JLR_CONTAINER).getChild("vehiclecontainer").getChild("vehicles");
+		List<String> list = new ArrayList<>();
+        Iterator<Resource> childResources=containerResource.listChildren();
+        while (childResources.hasNext()) {
+            Resource child = childResources.next();
+            ValueMap properties = child.adaptTo(ValueMap.class);
+            if(StringUtils.compare(vehicledropdown, properties.get(JcrResourceConstants.SLING_RESOURCE_TYPE_PROPERTY, String.class))==0) { 
+            	Resource child2 = child.getChild("vehicleFamily");
+                Iterator<Resource> resource2=child2.listChildren();
+                while(resource2.hasNext()) {
+                	Resource child3 = resource2.next();
+                	Iterator<Resource> resource3=child3.listChildren();
+                	while(resource3.hasNext()) {
+                		Resource child4 = resource3.next();
+                		Iterator<Resource> resource4=child4.listChildren();
+                		ValueMap properties2 = child4.adaptTo(ValueMap.class);
+                		while(resource4.hasNext()) {
+                			Resource child5 = resource4.next();
+                    		ValueMap properties3 = child5.adaptTo(ValueMap.class);
+                    		list.add(properties3.get("vehicleTitle", String.class));
+                    		logger.info("Inside 2ifff {}",properties3);
+                		}
+                	}
+                }
+            	}
             }
         return list;
 	}
