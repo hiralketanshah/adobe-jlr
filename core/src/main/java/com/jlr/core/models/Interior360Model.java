9efeabee5a6126e20d3ad2a7df49a233d625019d
Kulbir Singh Mann <tec86102@adobe.com>
2021-05-05 16:12:32 +0530

    [feature/kestrel-37] Added comments

--- core/src/main/java/com/jlr/core/models/Interior360Model.java
+++ core/src/main/java/com/jlr/core/models/Interior360Model.java
@@ -6,7 +6,13 @@ import com.jlr.core.pojos.CTAPojo;
 
 public interface Interior360Model {
 
+	/**
+	 * Return CTA List
+	 */
 	public List<CTAPojo> getCtaList();
 	
+	/**
+	 * Return images folder path
+	 */
 	public String getFolderPath();
 }
