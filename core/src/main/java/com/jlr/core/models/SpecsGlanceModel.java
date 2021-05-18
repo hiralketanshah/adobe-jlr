19497b4fa29030c686b9bd58b3c8ed2f4db3d299
Kulbir Singh Mann <tec86102@adobe.com>
2021-05-18 23:43:35 +0530

    [feature/kestrel-111] First cut integration

---
+++ core/src/main/java/com/jlr/core/models/SpecsGlanceModel.java
@@ -0,0 +1,10 @@
+package com.jlr.core.models;
+
+import java.util.List;
+
+import com.jlr.core.internal.models.v1.SpecsGlanceItem;
+
+public interface SpecsGlanceModel {
+
+	 public List<SpecsGlanceItem> getDataItems();
+}
