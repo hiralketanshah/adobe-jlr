6b2ad907f85a9f4e759776de81655e5db7459a8c
Gopinath <tec38572@adobe.com>
2021-05-31 23:34:24 +0530

    KESTREL-788-[content card NPE fix added]

--- core/src/main/java/com/jlr/core/models/ContentCardListModel.java
+++ core/src/main/java/com/jlr/core/models/ContentCardListModel.java
@@ -1,132 +1,76 @@
 package com.jlr.core.models;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
-import javax.inject.Inject;
+import org.apache.commons.lang.StringUtils;
+import org.osgi.annotation.versioning.ConsumerType;
 
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.api.resource.ValueMap;
-import org.apache.sling.models.annotations.DefaultInjectionStrategy;
-import org.apache.sling.models.annotations.Model;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.jlr.core.constants.CommonConstants;
-import com.jlr.core.internal.models.v1.GlobalModelImpl;
 import com.jlr.core.pojos.CTAPojo;
-import com.jlr.core.utils.CtaUtils;
-import com.jlr.core.utils.LinkUtils;
 
 /**
- * The Class ContentCardListModel.
- *
- * @author Adobe
+ * The Interface ContentCardListModel.
  */
-@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-public class ContentCardListModel extends GlobalModelImpl {
+@ConsumerType
+public interface ContentCardListModel extends GlobalModel {
 
-    /** The resource resolver. */
-    @Inject
-    private ResourceResolver resourceResolver;
-
-    /** The resource. */
-    @Inject
-    private Resource resource;
-
-    /** The assest type. */
-    @Inject
-    private String assestType;
-
-    /** The first cta link. */
-    @Inject
-    private String firstCtaLink;
-
-    /** The price. */
-    @Inject
-    private String price;
-
-    /** The gallery link. */
-    @Inject
-    private String galleryLink;
-
-    /** The cta list. */
-    @Inject
-    private Resource ctaList;
-
-    /** The logger. */
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    /** The list. */
-    List<CTAPojo> list = new ArrayList<>();
-    String priceConfigValue;
-
-    /**
-     * Gets the cta list.
-     *
-     * @return the cta list
-     */
-    public List<CTAPojo> getCtaList() {
-        if (null != ctaList && ctaList.hasChildren()) {
-            list = CtaUtils.createCtaList(ctaList, super.getHeaderCopy(), resourceResolver);
-        }
-        return list;
+	/**
+	 * Gets the cta list.
+	 *
+	 * @return the cta list
+	 */
+	default List<CTAPojo> getCtaList() {
+        return new ArrayList<>();
+    }
+	
+	/**
+	 * Gets the assest type.
+	 *
+	 * @return the assest type
+	 */
+	default String getAssestType() {
+        return StringUtils.EMPTY;
+    }
+	
+	/**
+	 * Gets the first cta link.
+	 *
+	 * @return the first cta link
+	 */
+	default String getFirstCtaLink() {
+        return StringUtils.EMPTY;
+    }
+	
+	/**
+	 * Gets the price config value.
+	 *
+	 * @return the price config value
+	 */
+	default String getPriceConfigValue() {
+        return StringUtils.EMPTY;
+    }
+	
+	/**
+	 * Gets the gallery link.
+	 *
+	 * @return the gallery link
+	 */
+	default String getGalleryLink() {
+        return StringUtils.EMPTY;
     }
 
-    /**
-     * Gets the assest type.
-     *
-     * @return the assest type
-     */
-    public String getAssestType() {
-        return assestType;
-    }
+	/**
+	 * Sets the price.
+	 *
+	 * @param price the new price
+	 */
+	void setPrice(String price);
 
-    /**
-     * Gets the first cta link.
-     *
-     * @return the first cta link
-     */
-    public String getFirstCtaLink() {
-        Resource contentCta = resource.listChildren().next();
-        Iterator<Resource> contentCtaRes = contentCta.listChildren();
-        Resource contentCtaLink = contentCtaRes.next();
-        ValueMap properties = contentCtaLink.adaptTo(ValueMap.class);
-        String firstCtaLink = properties.get(CommonConstants.PN_CTA_LINK, String.class);
-        return LinkUtils.appendLinkExtension(firstCtaLink, resourceResolver);
-    }
-
-    /**
-     * Gets the price.
-     *
-     * @return the price
-     */
-    @Override
-    public String getPrice() {
-        return price;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
-    }
-
-    public String getPriceConfigValue() {
-        return priceConfigValue;
-    }
-
-    public void setPriceConfigValue(String priceConfigValue) {
-        this.priceConfigValue = priceConfigValue;
-    }
-
-    /**
-     * Gets the gallery link.
-     *
-     * @return the gallery link
-     */
-    public String getGalleryLink() {
-        return LinkUtils.appendLinkExtension(galleryLink, resourceResolver);
-    }
+	/**
+	 * Sets the price config value.
+	 *
+	 * @param priceConfigValue the new price config value
+	 */
+	void setPriceConfigValue(String priceConfigValue);
 
 }
