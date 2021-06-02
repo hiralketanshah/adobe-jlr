8140c748be37826a28e0d43f0337de5e62a1318b
Hiral Shah <hishah@adobe.com>
2021-06-02 17:27:34 +0530

    [feature/kestrel-33] Updated backend code

--- core/src/main/java/com/jlr/core/internal/services/DerivativeImpl.java
+++ core/src/main/java/com/jlr/core/internal/services/DerivativeImpl.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.api.resource.ValueMap;
 import org.osgi.service.component.annotations.Component;
@@ -15,10 +14,10 @@ import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.day.cq.commons.jcr.JcrConstants;
-import com.jlr.core.constants.CommonConstants;
+import com.jlr.core.constants.DerivativeConstants;
 import com.jlr.core.internal.models.v1.DerivativeCardModelImpl;
 import com.jlr.core.services.Derivative;
+import com.jlr.core.utils.DerivativeUtils;
 
 @Component(service = Derivative.class)
 public class DerivativeImpl implements Derivative {
@@ -31,37 +30,25 @@ public class DerivativeImpl implements Derivative {
 
     @Override
     public String getListOfDerivativeDropdown(SlingHttpServletRequest request, String path) {
-        ResourceResolver resolver = null;
         String dropdownName = StringUtils.EMPTY;
-        resolver = request.getResourceResolver();
-        Resource derivativeCardResource = resolver.getResource(path);
-        if (null != derivativeCardResource) {
-            Resource container = derivativeCardResource
-                    .getChild(JcrConstants.JCR_CONTENT + CommonConstants.FORWARD_SLASH + CommonConstants.JLR_ROOT
-                            + CommonConstants.FORWARD_SLASH + CommonConstants.JLR_CONTAINER + "/cardcontainer");
-            if (null != container) {
-                ValueMap prop = container.adaptTo(ValueMap.class);
-                if (null != prop && prop.containsKey("dropdownName")) {
-                    dropdownName = prop.get("dropdownName", String.class);
-                }
+        Resource container = DerivativeUtils.getCardContainerResource(request, path);
+        if (null != container) {
+            ValueMap prop = container.adaptTo(ValueMap.class);
+            if (null != prop && prop.containsKey(DerivativeConstants.PN_DROPDOWN_NAME)) {
+                dropdownName = prop.get(DerivativeConstants.PN_DROPDOWN_NAME, String.class);
             }
-
         }
-
         return dropdownName;
     }
 
     @Override
     public List<DerivativeCardModelImpl> getDerivativeCard(SlingHttpServletRequest request, String path) {
-        ResourceResolver resolver = request.getResourceResolver();
+        Resource container = DerivativeUtils.getCardContainerResource(request, path);
         List<DerivativeCardModelImpl> listOfCards = new ArrayList<>();
-        Resource derivativeCardResource = resolver.getResource(path);
-        if (null != derivativeCardResource) {
-            Resource containerResource = derivativeCardResource.getChild(JcrConstants.JCR_CONTENT
-                    + CommonConstants.FORWARD_SLASH + CommonConstants.JLR_ROOT + CommonConstants.FORWARD_SLASH
-                    + CommonConstants.JLR_CONTAINER + CommonConstants.FORWARD_SLASH + "cardcontainer/parsys");
-            if (null != containerResource) {
-                Iterator<Resource> containerResourceIterator = containerResource.listChildren();
+        if (null != container) {
+            Resource parsysResource = container.getChild(DerivativeConstants.NN_PARSYS);
+            if (null != parsysResource) {
+                Iterator<Resource> containerResourceIterator = parsysResource.listChildren();
                 while (containerResourceIterator.hasNext()) {
                     Resource cardResource = containerResourceIterator.next();
                     if (null != cardResource) {
@@ -69,9 +56,7 @@ public class DerivativeImpl implements Derivative {
                         listOfCards.add(card);
                     }
                 }
-
             }
-
         }
         return listOfCards;
     }
--- core/src/main/java/com/jlr/core/utils/DerivativeUtils.java
+++ core/src/main/java/com/jlr/core/utils/DerivativeUtils.java
@@ -4,8 +4,14 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ValueMap;
+
+import com.day.cq.commons.jcr.JcrConstants;
+import com.jlr.core.constants.CommonConstants;
+import com.jlr.core.constants.DerivativeConstants;
 
 public class DerivativeUtils {
 
@@ -15,18 +21,31 @@ public class DerivativeUtils {
     public static Map<String, String> getDataMap(Resource engine) {
         Map<String, String> mapOfData = new LinkedHashMap<>();
 
-        Resource dataListResource = engine.getChild("dataList");
+        Resource dataListResource = engine.getChild(DerivativeConstants.NN_ENGINE_DATA_LIST);
         if (null != dataListResource) {
             Iterator<Resource> data = dataListResource.listChildren();
             while (data.hasNext()) {
                 Resource dataResource = data.next();
                 ValueMap properties = dataResource.adaptTo(ValueMap.class);
-                mapOfData.put(properties.get("heading", String.class), properties.get("data", String.class));
+                mapOfData.put(properties.get(DerivativeConstants.PN_ENGINE_HEADING, String.class),
+                        properties.get(DerivativeConstants.PN_ENGINE_DATA, String.class));
             }
         }
 
         return mapOfData;
 
+    }
+
+    public static Resource getCardContainerResource(SlingHttpServletRequest request, String path) {
+        ResourceResolver resolver = request.getResourceResolver();
+        Resource container = null;
+        Resource derivativeCardResource = resolver.getResource(path);
+        if (null != derivativeCardResource) {
+            container = derivativeCardResource.getChild(JcrConstants.JCR_CONTENT + CommonConstants.FORWARD_SLASH
+                    + CommonConstants.JLR_ROOT + CommonConstants.FORWARD_SLASH + CommonConstants.JLR_CONTAINER
+                    + CommonConstants.FORWARD_SLASH + DerivativeConstants.NN_CARD_CONTAINER);
+        }
+        return container;
     }
 
 }
