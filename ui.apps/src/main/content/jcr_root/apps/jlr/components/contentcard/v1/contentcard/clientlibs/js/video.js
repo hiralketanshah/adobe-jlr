859e363f966d305f347c897acb2883a881538c2c
Karvannan Pandurangan <KP00517571@techmahindra.com>
2021-06-04 17:42:45 +0530

    fixed js issue

--- ui.apps/src/main/content/jcr_root/apps/jlr/components/contentcard/v1/contentcard/clientlibs/js/video.js
+++ ui.apps/src/main/content/jcr_root/apps/jlr/components/contentcard/v1/contentcard/clientlibs/js/video.js
@@ -7,19 +7,23 @@ $('.ContentCard__Youtube').click(function () {
     $('.contentCardVideo').attr('src', url);
     modal.style.display = "block";
 });
-closeBtn.onclick = function () {
-    $('.contentCardVideo').each(function () {
-        var el_src = $(this).attr("src");
-        $(this).attr("src", el_src);
-    });
-    modal.style.display = "none";
-}
-window.onclick = function (event) {
-    if (event.target == modal) {
+if (closeBtn) {
+    closeBtn.onclick = function () {
         $('.contentCardVideo').each(function () {
             var el_src = $(this).attr("src");
             $(this).attr("src", "");
         });
         modal.style.display = "none";
+    }
+}
+window.onclick = function (event) {
+    if (modal) {
+        if (event.target == modal) {
+            $('.contentCardVideo').each(function () {
+                var el_src = $(this).attr("src");
+                $(this).attr("src", el_src);
+            });
+            modal.style.display = "none";
+        }
     }
 }
