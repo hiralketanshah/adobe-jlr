b334414f12fba55dbdf27bf8ea5c4d58fde7954c
Hiral Shah <hishah@adobe.com>
2021-06-07 17:27:51 +0530

    [feature/kestrel-33] Updated metaTitle and metaDescription for the tabbed pages

--- core/src/main/java/com/jlr/core/constants/DerivativeConstants.java
+++ core/src/main/java/com/jlr/core/constants/DerivativeConstants.java
@@ -7,7 +7,11 @@ package com.jlr.core.constants;
  */
 public class DerivativeConstants {
 
+    public static final String RT_DERIVATIVE = "jlr/components/derivative/v1/derivativecontainer";
+
     public static final String PN_TAB_HEADING = "tabHeading";
+
+    public static final String NN_TAB_LIST = "tabList";
 
     public static final String DYNAMIC_STATIC_PATH = "dr";
 
@@ -32,6 +36,10 @@ public class DerivativeConstants {
     public static final String PN_ENGINE_HEADING = "heading";
 
     public static final String PN_ENGINE_DATA = "data";
+
+    public static final String PN_META_TITLE = "metaTitle";
+
+    public static final String PN_META_DESCRIPTION = "metaDescription";
 
     public static final String NN_CARD_CONTAINER = "cardcontainer";
 
