849f3ee570a140472c94d82190b3f916b0b33df8
sugumar <sugumar@adobe.com>
2021-06-13 09:52:36 +0530

    [Feature/Kestrel-73] - Added separate WF bundle

---
+++ core.wf/src/main/java/com/jlr/wf/core/utils/WorkflowUtils.java
@@ -0,0 +1,15 @@
+package com.wf.jlr.core.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * The type Workflow utils.
+ */
+public class WorkflowUtils {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WorkflowUtils.class);
+
+
+}
