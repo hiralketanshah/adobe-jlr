e11cd36abf2e488faab82d038a778b4b23ec5e62
Gopinath D <tec38572@adobe.com>
2021-06-30 17:11:16 +0530

    KESTREL-1043-[alternative text java modified] (#638)
    * KESTREL-1043-[alternative text java modified]
    
    * KESTREL-1043-[grey theme removed for vehicle summary]

--- core/src/main/java/com/jlr/core/internal/models/v1/GlobalModelImpl.java
+++ core/src/main/java/com/jlr/core/internal/models/v1/GlobalModelImpl.java
@@ -249,13 +249,13 @@ public class GlobalModelImpl implements GlobalModel {
 		if (isDecorative) {
 			return null;
 		} else {
-			if (imageAlt != null && !imageAlt.isEmpty() && altTextFromDAM == Boolean.TRUE) {
+			if (imageAlt != null && altTextFromDAM == Boolean.TRUE) {
 				altDAMText = imageAlt;
-			} else if (imageAlt != null && !imageAlt.isEmpty() && altTextFromDAM == Boolean.FALSE) {
+			} else if (imageAlt != null && altTextFromDAM == Boolean.FALSE) {
 				altDAMText = imageAlt;
-			} else if (imageAlt != null && imageAlt.isEmpty() && altTextFromDAM == Boolean.FALSE) {
+			} else if (imageAlt == null && altTextFromDAM == Boolean.FALSE) {
 				altDAMText = StringUtils.EMPTY;
-			} else if (altTextFromDAM != null && altTextFromDAM == Boolean.TRUE) {
+			} else if (imageAlt == null && altTextFromDAM != null && altTextFromDAM == Boolean.TRUE) {
 				altDAMText = damAltText;
 			}
 		}
