6d12ff04d86aae34dee63b77148d48721b990983
Snehil Gupta <41028+tec22702@users.noreply.git.corp.adobe.com>
2021-07-02 14:58:50 +0530

    [Feature/kestrel 1071] Junit: TcoModelImpl VeticalsliderItem ReadytoGoBar (#662)
    * Junit Classes
    
    * Junit coverage
    
    Co-authored-by: tec22702 <SG00681400@techmahindra.com>

---
+++ core/src/test/java/com/jlr/core/internal/models/v1/VerticalSliderItemTest.java
@@ -0,0 +1,47 @@
+package com.jlr.core.internal.models.v1;
+
+import com.jlr.core.pojos.CTAPojo;
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+import org.apache.sling.api.resource.Resource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * The Class VerticalSliderItemTest.
+ *
+ */
+@ExtendWith(AemContextExtension.class)
+public class VerticalSliderItemTest extends GlobalModelImplTest {
+
+    /** The VerticalSliderItem */
+    private VerticalSliderItem verticalSliderItem;
+
+    /**
+     * Sets the up.
+     *
+     * @param context the new up
+     * @throws Exception the exception
+     */
+    @BeforeEach
+    public void setUp(AemContext context) throws Exception {
+        context.load().json("/content/jlr/verticalSlider/verticalslideritem.json", "/content/jlr/verticalslideritem.html");
+        Resource resource = context.resourceResolver().getResource("/content/jlr/verticalslideritem.html");
+        verticalSliderItem = resource.adaptTo(VerticalSliderItem.class);
+    }
+
+    /**
+     * Test cta properties.
+     */
+    @Override
+    @Test
+    void testCtaProperties() {
+        List<CTAPojo> list = verticalSliderItem.getCtaList();
+        assertEquals(1, list.size());
+    }
+}
