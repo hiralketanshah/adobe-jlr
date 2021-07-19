2c1911967fb9ab6e9cc748a83a4defdba5a54383
Koripelli Vijay Kumar <41023+tec10723@users.noreply.git.corp.adobe.com>
2021-07-19 16:55:53 +0530

    [feature/kestreel-1067]-[Junit] - Exterior360ModelImpl
    [feature/kestreel-1067]-[Junit] - Exterior360ModelImpl

--- core/src/test/java/com/jlr/core/internal/models/v1/Exterior360ModelImplTest.java
+++ core/src/test/java/com/jlr/core/internal/models/v1/Exterior360ModelImplTest.java
@@ -1,26 +1,23 @@
 package com.jlr.core.internal.models.v1;
-
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
 import org.apache.sling.api.resource.Resource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 /**
  * The Class Exterior360ModelImplTest.
  *
  */
 @ExtendWith(AemContextExtension.class)
-class Exterior360ModelImplTest {
-
+class Exterior360ModelImplTest extends GlobalModelImplTest{
     /** The Exterior360ModelImpl */
     private Exterior360ModelImpl exterior360Model;
-
     /**
      * Sets the up.
      *
@@ -31,6 +28,7 @@ class Exterior360ModelImplTest {
     public void setUp(AemContext context) throws Exception {
         context.load().json("/content/jlr/exterior360/exterior360.json", "/content/jlr/exterior360.html");
         Resource resource = context.resourceResolver().getResource("/content/jlr/exterior360.html");
+        context.load().json("/content/jlr/exterior360/image.json", "/content/dam/landrover");
         exterior360Model = resource.adaptTo(Exterior360ModelImpl.class);
     }
     /**
@@ -38,17 +36,18 @@ class Exterior360ModelImplTest {
      */
     @Test
     void testProperties() {
-       // assertEquals("/content/dam/landrover", exterior360Model.getFolderPath());
-
+        assertEquals("/content/dam/landrover", exterior360Model.getFolderPath());
+        assertTrue(exterior360Model.getIsSlider());
+        assertEquals("arial_text", exterior360Model.getAriaLabel());
+        assertEquals("video", exterior360Model.getAssetType());
     }
     /**
-     * Test column.
+     * Test properties.
      */
-    void testGetImageList() {
-
-        List<String> imageList = new ArrayList<>();
-        assertEquals(1, imageList.toString());
-
+    @Test
+    void testImageList() {
+        List<String> listImage = Collections.singletonList(exterior360Model.getImageList());
+        assertEquals(1, listImage.size());
     }
 
-}
+    }
