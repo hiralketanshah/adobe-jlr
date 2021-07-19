a1c1cd7de1534cf3a3a8502135f56e9b499a0af7
Snehil Gupta <41028+tec22702@users.noreply.git.corp.adobe.com>
2021-07-19 15:36:26 +0530

    [Feature/kestrel 1148]: Junit coverage (#763)
    * Junit classes
    
    * DAMFolderUtilsTest
    
    Co-authored-by: tec22702 <SG00681400@techmahindra.com>

--- core/src/test/java/com/jlr/core/internal/models/v1/HeroItemModelImplTest.java
+++ core/src/test/java/com/jlr/core/internal/models/v1/HeroItemModelImplTest.java
@@ -1,17 +1,18 @@
 package com.jlr.core.internal.models.v1;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
 
 import com.jlr.core.pojos.FooterPojo;
+import com.jlr.core.utils.ComponentPositionUtils;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 
 import com.day.cq.commons.inherit.InheritanceValueMap;
 import com.day.cq.wcm.api.Page;
@@ -22,6 +23,7 @@ import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
 
 import javax.jcr.Node;
+import javax.jcr.RepositoryException;
 
 /**
  * The Class HeroCarouselModelImplTest.
@@ -65,8 +67,15 @@ class HeroItemModelImplTest extends GlobalModelImplTest {
     }
 
     @Test
-    void testProperties(){
+    void testProperties() throws RepositoryException {
         assertEquals("caveatText",heroItemModel.getCaveat());
+        when(currentPage.getPath()).thenReturn("/content/jlr/page");
+        try (MockedStatic<ComponentPositionUtils> mock = Mockito.mockStatic(ComponentPositionUtils.class)) {
+            mock.when(() -> {
+                ComponentPositionUtils.getComponentPosition(Mockito.any(String.class), Mockito.any(Node.class),Mockito.any(ResourceResolver.class));
+            }).thenReturn(true);
+            assertEquals(true,heroItemModel.getFirstPosition());
+        }
     }
 
     /**
